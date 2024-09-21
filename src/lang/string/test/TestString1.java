70b706cd1bed39dc67efcd12691b4ff2378dfd27
hjn42 <hjn42515@gmail.com>
2024-09-21 23:13:20 +0900

    init commit

---
+++ src/lang/string/test/TestString1.java
@@ -0,0 +1,12 @@
+package lang.string.test;
+
+public class TestString1 {
+
+    public static void main(String[] args) {
+        String ur1 = "https://www.example.com";
+        boolean result = ur1.startsWith("https://");
+        System.out.println(result);
+
+
+    }
+}

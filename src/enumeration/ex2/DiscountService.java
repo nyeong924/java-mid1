390a1d2f1e45410e8570a28c3d917b8f53369629
hjn42 <hjn42515@gmail.com>
2024-10-15 13:42:55 +0900

    init commit

---
+++ src/enumeration/ex2/DiscountService.java
@@ -0,0 +1,20 @@
+package enumeration.ex2;
+
+public class DiscountService {
+
+    public int discount(ClassGrade classGrade, int price) {
+        int discountPercent = 0;
+
+        if (classGrade == ClassGrade.BASIC) {
+            discountPercent = 10;
+        } else if (classGrade == ClassGrade.GOLD) {
+            discountPercent = 20;
+        } else if (classGrade == ClassGrade.DIAMOND) {
+            discountPercent = 30;
+        } else {
+            System.out.println("할인이 적용되지 않습니다.");
+        }
+
+        return price * discountPercent / 100;
+    }
+}
---
+++ src/enumeration/ex3/ClassGradeEx3_2.java
@@ -0,0 +1,17 @@
+package enumeration.ex3;
+
+import enumeration.ex2.DiscountService;
+
+public class ClassGradeEx3_2 {
+
+    public static void main(String[] args) {
+        int price = 10000;
+
+        DiscountService discountService = new DiscountService();
+
+        /*Grade grade = new Grade(); //enum 생성 불가
+        int result = discountService.discount(grade, price);
+        System.out.println("result price: " + result);*/
+
+    }
+}

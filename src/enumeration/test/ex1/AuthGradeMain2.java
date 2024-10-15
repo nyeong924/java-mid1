4b92668ba86c04464100df8fdb28c0232f1b3330
hjn42 <hjn42515@gmail.com>
2024-10-15 16:15:40 +0900

    init commit

---
+++ src/enumeration/test/ex1/AuthGradeMain2.java
@@ -0,0 +1,28 @@
+package enumeration.test.ex1;
+
+import java.util.Scanner;
+
+public class AuthGradeMain2 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("당신의 등급을 입력하세요 [GUEST, LOGIN, ADMIN]: ");
+        String mg = sc.nextLine();
+
+        AuthGrade grade = AuthGrade.valueOf(mg.toUpperCase());
+        System.out.println("당신의 등급은 " + grade.getDescription() + "입니다.");
+
+        System.out.println("==메뉴 목록==");
+        if (grade.getLevel() > 0) {
+            System.out.println("- 메인 화면");
+        }
+        if (grade.getLevel() > 1) {
+            System.out.println("- 이메일 관리 화면");
+        }
+        if (grade.getLevel() > 2) {
+            System.out.println("- 관리자 화면");
+        }
+    }
+}
+
